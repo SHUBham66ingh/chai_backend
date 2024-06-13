b830ee9b093411afa9c10ca54bd57e0b158c6d87
Shubham singh <shubham1041singh@gmail.com>
2024-06-13 22:48:48 +0530

    .......

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express= require('express');
+import express from 'express';
 
 const app=express();
 
@@ -6,7 +6,42 @@ app.get('/' , (req,res)=>{
   res.send('server is ready')  
 })
 
-const PORT = process.env.PORT
+app.get('/jokes' , (req,res)=>{
+    const jokes = [
+        {
+            id:1,
+            title:'A joke',
+            content : 'this is a joke'
+        },
+        {
+            id:2,
+            title:'Another joke',
+            content : 'this is a joke'
+        },
+        {
+            id:3,
+            title:'A third joke',
+            content : 'this is a third joke'
+        },
+        {
+            id:4,
+            title:'A fourth joke',
+            content : 'this is a fourth joke'
+        },
+        {
+            id:5,
+            title:'A fifth joke',
+            content : 'this is a fifth joke'
+        },
+    ]
+    res.send(jokes);
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT , ()=>{
+     console.log(`server started at port ${PORT}`)
+})
 
 
 

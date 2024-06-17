6de202383c3460bc2d11d631b47272a997a90b36
Shubham singh <shubham1041singh@gmail.com>
2024-06-17 23:56:10 +0530

    ........

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,23 @@
 import {asyncHandler}  from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
-   // get user from frontend  
+   // get user from frontend
+   // validation - not empty
+   //  check if uer already exists: useranam, email
+   // check for images and avatar
+   // upload them to cloudinary , avatar
+   // create user onject - create entry in db
+   // remove password and refresh token 
+   //check for user creation
+   // return res
+
+     const {fullName , email , username , password} =req.body
+     console.log("email" , email)
+
+
+
+
+
 })
 
 export{registerUser,}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,24 @@ import {Router}  from "express"
 import { registerUser } from "../controllers/user.controller.js"
 
 const router = Router()
+import {upload} from "../middlewares/multer.middleware.js"
 
-
-router.route("/register").post(registerUser)
+router.route("/register").
+post
+(
+    upload.fields([
+         {
+            name: "avaatar",
+            maxCount : 1
+         },
+         {
+              name : "coverImage",
+              maxCount : 1
+         }
+    ]),
+    registerUser
+    
+)
 
 
 export default router
